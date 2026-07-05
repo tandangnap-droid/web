46a87d516e2848dcd2e95746556d000ef7a9d49e
tandangnap-droid <tandangnap@gmail.com>
2026-07-05 15:02:17 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 function saveTokens() {
-    const token = document.getElementById('inputToken1').value;
+    const token = document.getElementById('inputToken1').value.trim();
+    if (!token) return alert("Vui lòng nhập token!");
+    
+    // Lưu tạm vào localStorage (Lưu ý: Chỉ dùng cho test)
     localStorage.setItem('user_token', token);
-    alert('Đã lưu!');
+    alert('✅ Đã lưu token vào trình duyệt!');
 }
